a3e7dbd1d9d4b6318217f7c6bbf3f008ccdfc6aa
xtalie <esthernwobodo50@gmail.com>
2023-12-04 12:43:03 -0800

    first commit

---
+++ data.js
@@ -0,0 +1,388 @@
+import { createDate } from "./helpers.js";
+ 
+ 
+ export const taskDetails = {
+  taskList: [
+    {
+      sn: Math.random(),
+      description: "Do some shopping at mixLand shopping mall",
+      status: "done",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "	Attend coding with George class",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+
+    {
+      sn: Math.random(),
+      description: "Go to the saloon to make my hair",
+      status: "todo",
+      date: createDate(),
+      priority: "medium",
+    },
+
+    {
+      sn: Math.random(),
+      description: "Go to the saloon to make my hair",
+      status: "todo",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "	Go out and have some fun with friends",
+      status: "inprogress",
+      date: createDate(),
+      priority: "high",
+    },
+    {
+      sn:Math.random(),
+      description: "Create a schedule for the Next Day",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn:Math.random() ,
+      description: "	Read my books",
+      status: "done",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn: Math.random(),
+      description: "live my life",
+      status: "done",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "go to the church to pray",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+
+    {
+      sn: Math.random(),
+      description: "buy myself a good television",
+      status: "todo",
+      date: createDate(),
+      priority: "medium",
+    },
+
+    {
+      sn: Math.random(),
+      description: "wash my bed spread",
+      status: "todo",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "clean the house",
+      status: "inprogress",
+      date: createDate(),
+      priority: "high",
+    },
+    {
+      sn:Math.random(),
+      description: "practice my coding",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn:Math.random() ,
+      description: "watch videos on youTube",
+      status: "done",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn: Math.random(),
+      description: "charge my phone",
+      status: "done",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "do anything i like",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+
+    {
+      sn: Math.random(),
+      description: "go for a massage session",
+      status: "todo",
+      date: createDate(),
+      priority: "medium",
+    },
+
+    {
+      sn: Math.random(),
+      description: "dry my pillow",
+      status: "todo",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "clean the fridge",
+      status: "inprogress",
+      date: createDate(),
+      priority: "high",
+    },
+    {
+      sn:Math.random(),
+      description: "buy a duvet",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn:Math.random() ,
+      description: "stock my house",
+      status: "done",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn: Math.random(),
+      description: "be happy",
+      status: "done",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "attend functions",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+
+    {
+      sn: Math.random(),
+      description: "go for yoga session",
+      status: "todo",
+      date: createDate(),
+      priority: "medium",
+    },
+
+    {
+      sn: Math.random(),
+      description: "clean the fans",
+      status: "todo",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "go to the zoo",
+      status: "inprogress",
+      date: createDate(),
+      priority: "high",
+    },
+    {
+      sn:Math.random(),
+      description: "live health",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn:Math.random() ,
+      description: "buy a new towel",
+      status: "done",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn: Math.random(),
+      description: "pay bills",
+      status: "done",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "go to class",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+
+    {
+      sn: Math.random(),
+      description: "subcribe GOTV",
+      status: "todo",
+      date: createDate(),
+      priority: "medium",
+    },
+
+    {
+      sn: Math.random(),
+      description: "wash my bedspread",
+      status: "todo",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "buy a new bed",
+      status: "inprogress",
+      date: createDate(),
+      priority: "high",
+    },
+    {
+      sn:Math.random(),
+      description: "leave the house at leisure",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn:Math.random() ,
+      description: "buy perfume",
+      status: "done",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn: Math.random(),
+      description: "buy a new shoe",
+      status: "done",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "go on a life time trip",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+
+    {
+      sn: Math.random(),
+      description: "understand my notes",
+      status: "todo",
+      date: createDate(),
+      priority: "medium",
+    },
+
+    {
+      sn: Math.random(),
+      description: "Go to the saloon",
+      status: "todo",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "fun with friends",
+      status: "inprogress",
+      date: createDate(),
+      priority: "high",
+    },
+    {
+      sn:Math.random(),
+      description: "schedule for the Next Day",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn:Math.random() ,
+      description: "	Read my books",
+      status: "done",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn: Math.random(),
+      description: "mixLand shopping mall",
+      status: "done",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "Attend coding with George class",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+
+    {
+      sn: Math.random(),
+      description: "Go to the saloon to make my hair",
+      status: "todo",
+      date: createDate(),
+      priority: "medium",
+    },
+
+    {
+      sn: Math.random(),
+      description: "Go to the saloon to make my hair",
+      status: "todo",
+      date: createDate(),
+      priority: "high",
+    },
+
+    {
+      sn: Math.random(),
+      description: "	Go out and have some fun ",
+      status: "inprogress",
+      date: createDate(),
+      priority: "high",
+    },
+    {
+      sn:Math.random(),
+      description: "Create a schedule for the Next Day",
+      status: "inprogress",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn:Math.random() ,
+      description: "go for gym class",
+      status: "done",
+      date: createDate(),
+      priority: "low",
+    },
+    {
+      sn:Math.random() ,
+      description: "buy a sneaker",
+      status: "done",
+      date: createDate(),
+      priority: "low",
+    }
+  ]
+};
+
+
+// pagin
